a0218a712f180d9bc8834d2488e36c9c4a3e976b
Hyeon <Hyeon@Hyeon>
2018-11-01 17:51:02 +0900

    현:라이트코멘트컨트롤러 수정 - 댓글작성시 유저아이디 같이 넘겨줘서 보드리스트 뜨게해줌

--- project_SNS/src/main/java/com/team/springsns/comment/controller/WriteCommentController.java
+++ project_SNS/src/main/java/com/team/springsns/comment/controller/WriteCommentController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.team.springsns.comment.model.CommentDTO;
 import com.team.springsns.comment.service.WriteCommentService;
@@ -21,9 +23,17 @@ public class WriteCommentController {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public String writeComment(CommentDTO commentDTO) {
+	public ModelAndView writeComment(@RequestParam(value = "userId", required = false) String userId,
+								CommentDTO commentDTO) {
+		
+		ModelAndView modelAndView = new ModelAndView();
+		
+		modelAndView.addObject("userId", userId);
+		modelAndView.setViewName("redirect:/board/boardList");
+		
 		writeCommentService.writeComment(commentDTO);
-		return "redirect:/board/boardList";
+		
+		return modelAndView;
 	}
 
 }
